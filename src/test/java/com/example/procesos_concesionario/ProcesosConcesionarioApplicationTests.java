2ba2feb0bd8b1bd153175126eb79a7642f7705c5
SCIS3-PC18\SCIS3-18 <correo>
2023-03-02 20:50:41 -0500

    creación principal del proyecto

---
+++ src/test/java/com/example/procesos_concesionario/ProcesosConcesionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.procesos_concesionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesosConcesionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
